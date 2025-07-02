ff59db4befe0f0d93200a63628c2f6e03fc97d04
Simone Rossi Tisbeni <rsreds@users.noreply.github.com>
2025-07-02 11:14:19 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+from dash import Dash, html
+
+app = Dash(__name__)
+app.layout = html.Div("Hello from Dash deployed on OpenShift!")
+
+if __name__ == "__main__":
+    app.run_server(host="0.0.0.0", port=8080)
